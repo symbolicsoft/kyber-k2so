c3e0cc2fd92bc23afaecd28238e06bd88299da76
Nadim Kobeissi <nadim@symbolic.software>
2020-07-31 09:46:49 +0200

    Clarify documentation (#1)

--- params.go
+++ params.go
@@ -25,32 +25,32 @@ const paramsIndcpaSecretKeyBytesK2 int = 2 * paramsPolyBytes
 const paramsIndcpaSecretKeyBytesK3 int = 3 * paramsPolyBytes
 const paramsIndcpaSecretKeyBytesK4 int = 4 * paramsPolyBytes
 
-// Kyber512SKBytes is a constant representing the length of private keys in Kyber-512.
+// Kyber512SKBytes is a constant representing the byte length of private keys in Kyber-512.
 const Kyber512SKBytes int = paramsPolyvecBytesK2 + ((paramsPolyvecBytesK2 + paramsSymBytes) + 2*paramsSymBytes)
 
-// Kyber768SKBytes is a constant representing the length of private keys in Kyber-768.
+// Kyber768SKBytes is a constant representing the byte length of private keys in Kyber-768.
 const Kyber768SKBytes int = paramsPolyvecBytesK3 + ((paramsPolyvecBytesK3 + paramsSymBytes) + 2*paramsSymBytes)
 
-// Kyber1024SKBytes is a constant representing the length of private keys in Kyber-1024.
+// Kyber1024SKBytes is a constant representing the byte length of private keys in Kyber-1024.
 const Kyber1024SKBytes int = paramsPolyvecBytesK4 + ((paramsPolyvecBytesK4 + paramsSymBytes) + 2*paramsSymBytes)
 
-// Kyber512PKBytes is a constant representing the length of public keys in Kyber-512.
+// Kyber512PKBytes is a constant representing the byte length of public keys in Kyber-512.
 const Kyber512PKBytes int = paramsPolyvecBytesK2 + paramsSymBytes
 
-// Kyber768PKBytes is a constant representing the length of public keys in Kyber-768.
+// Kyber768PKBytes is a constant representing the byte length of public keys in Kyber-768.
 const Kyber768PKBytes int = paramsPolyvecBytesK3 + paramsSymBytes
 
-// Kyber1024PKBytes is a constant representing the length of public keys in Kyber-1024.
+// Kyber1024PKBytes is a constant representing the byte length of public keys in Kyber-1024.
 const Kyber1024PKBytes int = paramsPolyvecBytesK4 + paramsSymBytes
 
-// Kyber512CTBytes is a constant representing the length of ciphertexts in Kyber-512.
+// Kyber512CTBytes is a constant representing the byte length of ciphertexts in Kyber-512.
 const Kyber512CTBytes int = paramsPolyvecCompressedBytesK2 + paramsPolyCompressedBytesK2
 
-// Kyber768CTBytes is a constant representing the length of ciphertexts in Kyber-768.
+// Kyber768CTBytes is a constant representing the byte length of ciphertexts in Kyber-768.
 const Kyber768CTBytes int = paramsPolyvecCompressedBytesK3 + paramsPolyCompressedBytesK3
 
-// Kyber1024CTBytes is a constant representing the length of ciphertexts in Kyber-1024.
+// Kyber1024CTBytes is a constant representing the byte length of ciphertexts in Kyber-1024.
 const Kyber1024CTBytes int = paramsPolyvecCompressedBytesK4 + paramsPolyCompressedBytesK4
 
-// KyberSSBytes is a constant representing the length of shared secrets in Kyber.
+// KyberSSBytes is a constant representing the byte length of shared secrets in Kyber.
 const KyberSSBytes int = 32
